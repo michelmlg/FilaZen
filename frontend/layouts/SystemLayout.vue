bdcdf02bd5f6d63e99333f2ec2b8f2fd80ca51af
michelmlg <147891522+michelmlg@users.noreply.github.com>
2025-05-03 13:43:08 -0300

    fix: remove old styles

--- frontend/layouts/SystemLayout.vue
+++ frontend/layouts/SystemLayout.vue
@@ -15,21 +15,3 @@ export default {
       <router-view></router-view>
     </div>
 </template>
-
-<style>
-  /* :root{
-    --textVue: #08545e;
-    --backgroundVue: #d3dede;
-    --primaryVue: #37be81;
-    --secondaryVue: #1bb2d0;
-    --accentVue: #7270db;
-  } */
-/* 
-  body{
-    font-family: "Nunito", serif;
-    background-color: var(--backgroundVue);
-    color: var(--textVue);
-  } */
-
-
-</style>
