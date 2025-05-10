b028554fbca9ed05f7d0d0b5817d93c89bece104
Michel Lisboa <147891522+michelmlg@users.noreply.github.com>
2025-05-09 21:08:41 -0300

    Update authController.php

--- backend/controllers/authController.php
+++ backend/controllers/authController.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '../../../vendor/autoload.php';
 use Filazen\Backend\models\Auth;
-use Filazen\Backend\Database\db;
+use Filazen\Backend\database\db;
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
