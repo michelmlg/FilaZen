5de3deedb300bde5f4049db9f36c01cea20d54b7
Michel Lisboa <147891522+michelmlg@users.noreply.github.com>
2026-04-21 01:28:31 -0300

    feat: cria rota dedicada e controllers para gerenciamento de migrations da API

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,6 +5,7 @@ import customerRoutes from './customer.routes'
 import ticketRoutes from './ticket.routes'
 import tenantRoutes from './tenant.routes'
 import queueRoutes from './queue.routes'
+import migrationRoutes from './migration.routes'
 
 const router = Router()
 
@@ -14,5 +15,6 @@ router.use('/customers', customerRoutes)
 router.use('/tickets', ticketRoutes)
 router.use('/tenants', tenantRoutes)
 router.use('/queue', queueRoutes)
+router.use('/migration', migrationRoutes)
 
 export default router
